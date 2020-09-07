4ccf5c4b95a7b5007e632168c1ca354e50c6dc47
Bjornfot-afk <65647056+Bjornfot-afk@users.noreply.github.com>
2020-09-07 15:24:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const userinput1 = prompt ("Give me a number")
+const userinput2 = prompt ("Give me a secound number")
+
+alert(userinput1)
+alert(userinput2)
+
+console.log(userinput1)
+console.log (userinput2)
